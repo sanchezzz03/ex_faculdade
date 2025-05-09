af135f8604f84896917703eb4cfff40e7dbd5b15
Lucas Sanchez <vicentelucas384@gmail.com>
2025-05-09 20:14:19 -0300

    adicionando sequencia de fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -5,8 +5,9 @@ num_atual = 1
 
 tamanho = int(input("digite o tamanho da sequencia que deseja: "))
 
-for i in range(tamanho):
-    print(num_inicial)
+for i in range((tamanho-1)):
     proximo = num_inicial + num_atual
     num_inicial = num_atual
     num_atual = proximo
+
+print(num_inicial)
