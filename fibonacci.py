6c29efe273f43a9966195a358030c037df9033a5
Lucas Sanchez <vicentelucas384@gmail.com>
2025-05-09 20:18:28 -0300

    deixando comentário na sequência de fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -5,9 +5,11 @@ num_atual = 1
 
 tamanho = int(input("digite o tamanho da sequencia que deseja: "))
 
-for i in range((tamanho-1)):
+for i in range((tamanho)):
     proximo = num_inicial + num_atual
     num_inicial = num_atual
     num_atual = proximo
 
 print(num_inicial)
+# assim mostra apenas o número no indice que foi escolhido
+# caso deixe o print no inicio do loop mostrará a sequência inteira
